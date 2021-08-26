f6d5b1552e4f0d0dc2cda47043077ccb7161c3ea
rinat88 <87369074+rinat88@users.noreply.github.com>
2021-08-26 13:27:09 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,10 @@ public class Main {
                     break;
                 case "Ali" :
                     System.out.println("Hi " + "Ali");
+                    break;
+                    default :
+                    System.out.println("System is error");
+                    
 
 
             }
